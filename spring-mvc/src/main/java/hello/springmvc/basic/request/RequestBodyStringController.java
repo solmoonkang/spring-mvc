9fef0417679fb4d24978ac289eb18cab896a512e
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-02 18:47:45 +0900

    feat: Input, OutputStream, Reader를 사용해 구현

--- spring-mvc/src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
+++ spring-mvc/src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
@@ -9,6 +9,8 @@ import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.Writer;
 import java.nio.charset.StandardCharsets;
 
 @Slf4j
@@ -23,6 +25,17 @@ public class RequestBodyStringController {
         log.info("messageBody={}", messageBody);
 
         response.getWriter().write("OK");
+    }
 
+    /**
+     * InputStream(Reader): HTTP 요청 메시지 바디의 내용을 직접 조회
+     * OutputStream(Writer): HTTP 응답 메시지의 바디에 직접 결과 출력
+     */
+    @PostMapping("/request-body-string-v2")
+    public void requestBodyStringV2(InputStream inputStream, Writer responseWriter) throws IOException {
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+        log.info("messageBody={}", messageBody);
+
+        responseWriter.write("OK");
     }
 }
