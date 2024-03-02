329cccc559c323db671d9182a9700184abae6544
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-02 18:48:44 +0900

    feat: HttpEntity를 사용해 정보를 조회

--- spring-mvc/src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
+++ spring-mvc/src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.ServletInputStream;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -37,5 +38,22 @@ public class RequestBodyStringController {
         log.info("messageBody={}", messageBody);
 
         responseWriter.write("OK");
+    }
+
+    /**
+     * HttpEntity: HTTP header, body 정보를 편리하게 조회
+     * - 메시지 바디 정보를 직접 조회(@RequestParam X, @ModelAttribute X)
+     * - HttpMessageConverter 사용 -> StringHttpMessageConverter 적용
+     *
+     * 응답에서도 HttpEntity 사용 가능
+     * - 메시지 바디 정보 직접 반환(view 조회X)
+     * - HttpMessageConverter 사용 -> StringHttpMessageConverter 적용
+     */
+    @PostMapping("/request-body-string-v3")
+    public HttpEntity<String> requestBodyStringV3(HttpEntity<String> httpEntity) {
+        String messageBody = httpEntity.getBody();
+        log.info("messageBody={}", messageBody);
+
+        return new HttpEntity<>("OK");
     }
 }
