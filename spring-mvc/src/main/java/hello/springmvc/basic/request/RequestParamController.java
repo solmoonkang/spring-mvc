e96882a1743999d224ba9c9bf5223e9b026b011c
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-02 18:04:02 +0900

    feat: @RequestParam을 사용하여 요청 파라미터를 사용

--- spring-mvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ spring-mvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -5,6 +5,8 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
 
@@ -19,5 +21,20 @@ public class RequestParamController {
         log.info("username={}, age={}", username, age);
 
         response.getWriter().write("OK");
+    }
+
+    /**
+     * @RequestParam 사용
+     * - 파라미터 이름으로 바인딩
+     *
+     * @ResponseBody 추가
+     * - View 조회를 무시하고, HTTP message body에 직접 해당 내용 입력
+     */
+    @ResponseBody
+    @RequestMapping("/request-param-v2")
+    public String requestParamV2(@RequestParam("username") String memberName,
+                                 @RequestParam("age") int memberAge) {
+        log.info("username={}, age={}", memberName, memberAge);
+        return "OK";
     }
 }
